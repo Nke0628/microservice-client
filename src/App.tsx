c2cc446555a14b87784303a4daada5ca873d491f
Nke0628 <ooo_diva@yahoo.co.jp>
2023-05-14 12:02:29 +0900

    アプリケーション全体のグローバルな設定はAppProviderで管理するように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import "./App.css";
-import { Provider, Client, fetchExchange, cacheExchange } from "urql";
 import RouteConfig from "./routes/RouteConfig";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./components/common/ErrorFallback";
-
-const client = new Client({
-  url: "http://localhost:4000/graphql",
-  exchanges: [fetchExchange, cacheExchange],
-  suspense: true,
-});
+import AppProvider from "./provider/AppProvider";
 
 function App() {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Provider value={client}>
-        <RouteConfig></RouteConfig>
-      </Provider>
-    </ErrorBoundary>
+    <AppProvider>
+      <RouteConfig></RouteConfig>
+    </AppProvider>
   );
 }
 
---
+++ src/provider/AppProvider.tsx
@@ -0,0 +1,22 @@
+import { Provider, Client, fetchExchange, cacheExchange } from "urql";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "../components/common/ErrorFallback";
+
+const client = new Client({
+  url: process.env.REACT_APP_BFF_URL!,
+  exchanges: [fetchExchange, cacheExchange],
+  suspense: true,
+});
+
+type Props = {
+  children: React.ReactElement;
+};
+
+const AppProvider: React.FC<Props> = ({ children }) => {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Provider value={client}>{children}</Provider>
+    </ErrorBoundary>
+  );
+};
+export default AppProvider;
