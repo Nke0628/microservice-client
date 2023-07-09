c47f88e4799da62592856f29e948573b1b0733da
Nke0628 <ooo_diva@yahoo.co.jp>
2023-07-09 11:02:48 +0900

    レポート設定画面で更新されうる値をstate管理する

--- src/components/features/reportSetting/ReportSettingContainer.tsx
+++ src/components/features/reportSetting/ReportSettingContainer.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import {
+  FetchReportSettingQuery,
   useFetchReportSettingQuery,
   useSaveReportSettingMutation,
 } from "../../../graphql/generated/graphql";
-import { ReportSetting, ReportSettingDetails } from "./report-setting.type";
+import { ReportSetting } from "./report-setting.type";
 import ReportSettingPresenter from "./ReportSettingPresenter";
 
 const ReportSettingContainer: React.FC = () => {
@@ -19,9 +20,11 @@ const ReportSettingContainer: React.FC = () => {
     },
   });
 
-  // 表示用にレポート設定情報の変換
+  // 表示用にレポート設定情報の型変換
   const reportSetting = result.data!.reportSetting;
-  const convertReportSettingDetails = () => {
+  const convertReportSettingDetails = (
+    reportSetting: FetchReportSettingQuery["reportSetting"]
+  ) => {
     return reportSetting.reportSettingDetails.map((reportSettingDetail) => {
       return {
         inputFlg: reportSettingDetail.inputFlg,
@@ -33,74 +36,85 @@ const ReportSettingContainer: React.FC = () => {
     });
   };
 
-  // 更新用にレポート設定情報をステート管理
-  const [reportSettingDetails, setReportSettingDetails] =
-    useState<ReportSettingDetails>(convertReportSettingDetails());
-  const convertReportSetting = (): ReportSetting => {
+  const convertReportSetting = (
+    reportSetting: FetchReportSettingQuery["reportSetting"]
+  ): ReportSetting => {
     return {
       savedAt: reportSetting.savedAt,
       savedBy: reportSetting.saveUser.name,
-      reportSettingDetails,
+      reportSettingDetails: convertReportSettingDetails(reportSetting),
     };
   };
+
+  // 更新用にレポート設定情報をステート管理
+  const [reportSettingState, setReportSettingState] = useState<ReportSetting>(
+    convertReportSetting(reportSetting)
+  );
 
   const handleUpdateTextForm = (
     event: React.ChangeEvent<HTMLInputElement>,
     i: number
   ) => {
-    setReportSettingDetails(
+    setReportSettingState({
+      ...reportSettingState,
       // mapで更新対象オブジェクトのみを変更した新規配列を作成し、stateにセットする
-      reportSettingDetails.map((val, index) =>
-        index === i
-          ? { ...val, [event.target.name]: event.target.value }
-          : { ...val }
-      )
-    );
+      reportSettingDetails: reportSettingState.reportSettingDetails.map(
+        (val, index) =>
+          index === i
+            ? { ...val, [event.target.name]: event.target.value }
+            : { ...val }
+      ),
+    });
   };
 
   const handleUpdateCheckForm = (
     event: React.ChangeEvent<HTMLInputElement>,
     i: number
   ) => {
-    setReportSettingDetails(
+    setReportSettingState({
+      ...reportSettingState,
       // mapで更新対象オブジェクトのみを変更した新規配列を作成し、stateにセットする
-      reportSettingDetails.map((val, index) =>
-        index === i
-          ? { ...val, [event.target.name]: event.target.checked }
-          : { ...val }
-      )
-    );
+      reportSettingDetails: reportSettingState.reportSettingDetails.map(
+        (val, index) =>
+          index === i
+            ? { ...val, [event.target.name]: event.target.checked }
+            : { ...val }
+      ),
+    });
   };
 
   // レポート保存
-  const navigate = useNavigate();
   const [, saveReportSetting] = useSaveReportSettingMutation();
   const handleSaveReportSetting = () => {
     saveReportSetting({
       input: {
         termId: Number(targetTermId),
-        reportSettingDetail: reportSettingDetails.map((r) => {
-          return {
-            inputFlg: r.inputFlg,
-            positionLayerType: r.positionLayerType,
-            theme: r.theme,
-            charaNum: Number(r.charaNum),
-          };
-        }),
+        reportSettingDetail: reportSettingState.reportSettingDetails.map(
+          (r) => {
+            return {
+              inputFlg: r.inputFlg,
+              positionLayerType: r.positionLayerType,
+              theme: r.theme,
+              charaNum: Number(r.charaNum),
+            };
+          }
+        ),
       },
     }).then((result) => {
       if (result.error) {
         alert(result.error?.graphQLErrors[0].extensions.errorDetail);
       } else {
+        setReportSettingState(
+          convertReportSetting(result.data!.saveReportSetting)
+        );
         alert("レポート設定を保存しました");
-        window.location.reload();
       }
     });
   };
 
   return (
     <ReportSettingPresenter
-      reportSetting={convertReportSetting()}
+      reportSetting={reportSettingState}
       handleCheckUpdate={handleUpdateCheckForm}
       handleTextUpdate={handleUpdateTextForm}
       handleSaveReportSetting={handleSaveReportSetting}
